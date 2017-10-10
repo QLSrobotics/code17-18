1c25598b90d533bc319a228f78ed9ce006cc8bd8
William Ng <williamwu2049@outlook.com>
2017-10-10 16:49:11 -0700

    drive train V1.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by William on 9/19/17.
@@ -13,12 +14,12 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class DriveTrain extends LinearOpMode{
     //motor initialized here
     //comment out the motors' declaration not physically connected to the robot
+    private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFront;
     private DcMotor rightFront;
     private DcMotor leftBack;
     private DcMotor rightBack;
-    private DcMotor clawLeft;
-    private DcMotor clawRight;
+    private DcMotor claw;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -30,8 +31,36 @@ public class DriveTrain extends LinearOpMode{
         rightFront = hardwareMap.dcMotor.get("RF");
         leftBack = hardwareMap.dcMotor.get("LB");
         rightBack = hardwareMap.dcMotor.get("RB");
-        clawLeft = hardwareMap.dcMotor.get("CL");
-        clawRight = hardwareMap.dcMotor.get("CR");
+        claw = hardwareMap.dcMotor.get("CLAW");
+
+        waitForStart();
+        runtime.reset();
+
+        while(opModeIsActive()) {
+
+            //joystick driving
+            leftFront.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
+            leftBack.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x);
+            rightFront.setPower(-gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x);
+            rightBack.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
+
+            //controlling lift
+            if (gamepad1.dpad_up) {
+                claw.setPower(0.4);
+            }
+            if (gamepad1.dpad_down) {
+                claw.setPower(-0.4);
+            }
+
+            //updating robot status and display on driver station
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+
+            //refresh
+            telemetry.update();
+
+            //always call at the bottom in while(opModeIsActive)
+            idle();
+        }
 
     }
 }
