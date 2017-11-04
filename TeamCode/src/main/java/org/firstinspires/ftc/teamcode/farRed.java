72e8d161195c9fe605f0c827f33e5ad7cebcf46d
William Ng <williamwu2049@outlook.com>
2017-11-03 18:16:23 -0700

    update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/farRed.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by hima on 11/3/17.
+ */
+
+@TeleOp(name="farRed", group="Team11920")
+
+
+public class farRed extends LinearOpMode {
+
+    private ColorSensor colorSensorBack;    // Hardware Device Object
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFront;
+    private DcMotor rightFront;
+    private DcMotor leftBack;
+    private DcMotor rightBack;
+    private Servo clawColour;
+    private Servo clawFrontServo;
+    private double colourThreshold = 100;  //color boundry between blue and red
+    @Override
+    public void runOpMode() {
+
+        //mapping the motor in this program to the motor in robot configuration
+        leftFront = hardwareMap.dcMotor.get("LF");
+        rightFront = hardwareMap.dcMotor.get("RF");
+        leftBack = hardwareMap.dcMotor.get("LB");
+        rightBack = hardwareMap.dcMotor.get("RB");
+        clawColour = hardwareMap.servo.get("CC");
+        clawFrontServo = hardwareMap.servo.get("CFS");
+
+        float hsvValues[] = {0F,0F,0F};
+        final float values[] = hsvValues;
+        colorSensorBack = hardwareMap.get(ColorSensor.class, "sensor_color_back");
+        waitForStart();
+        while (opModeIsActive()) {
+
+            clawFrontServo.setPosition(-50);
+
+            //williams stuff
+
+            driveStraight(1,5);
+            turn(1,2);
+            driveStraight(1,2);
+            turn(-1,2);
+
+            clawFrontServo.setPosition(40);
+
+            telemetry.update();
+            idle();
+        }
+    }
+
+    private void driveStraight(double speed, int time) {
+        rightFront.setPower(-speed);
+        rightBack.setPower(-speed);
+        leftFront.setPower(speed);
+        leftBack.setPower(speed);
+        sleep(time);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+
+    }
+
+    //positive speed for left turn
+    //negative speed for right turn
+    private void turn(double speed, int time) {
+        rightFront.setPower(speed);
+        rightBack.setPower(speed);
+        leftFront.setPower(speed);
+        leftBack.setPower(speed);
+        sleep(time);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+    }
+    public void sleep(int i){
+        long initial_time = System.currentTimeMillis();
+        while(System.currentTimeMillis()-initial_time <i) {
+        }
+    }
+}
